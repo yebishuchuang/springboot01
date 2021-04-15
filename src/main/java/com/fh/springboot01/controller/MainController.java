93d9726f3b780908b79c807e147c798070b66561
xgaifh <xgaifh@163.com>
2021-04-15 23:38:56 +0800

    在MainController中添加一个test测试方法，用于向github提交

--- src/main/java/com/fh/springboot01/controller/MainController.java
+++ src/main/java/com/fh/springboot01/controller/MainController.java
@@ -16,4 +16,12 @@ public class MainController {
         System.out.println("mainController中的getList()方法执行了");
         return "list";
     }
+
+    /**
+     * 新增一个test方法用来测试相github中提交代码
+     * @return
+     */
+    public  String test(){
+        return null;
+    }
 }
